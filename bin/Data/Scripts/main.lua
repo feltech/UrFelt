6b12d04c3eef4c283f2e05ac4d421feffd5d8510
David Feltell <dave@feltell.net>
2017-09-13 22:20:45 +0100

    Removed lua files transpiled from moonscript from repo

--- bin/Data/Scripts/main.lua
+++
@@ -1,3 +0,0 @@
-local root = GetFileSystem().GetProgramDir()
-package.path = root .. 'Data/Scripts/?.lua;' .. root .. 'Data/Scripts/lib/?.lua;' .. root .. 'Data/Scripts/lib/?/init.lua;'
-return require('test')
